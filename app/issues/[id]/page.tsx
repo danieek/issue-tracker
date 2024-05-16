55fc959774e59e99f59fb5ab02401827546294a0
danieek <daniel.eunchong.kim@gmail.com>
2024-05-16 11:35:00 -0700

    fix spacing on issue detail page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -18,14 +18,14 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Grid columns={{ initial: "1", md: "2" }} gap={{ initial: "5", md: "0" }}>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
       <Box>
         <Heading>{issue.title}</Heading>
         <Flex gap="3" my="2">
           <IssueStatusBadge status={issue.status} />
           <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
-        <Card className="prose" mt="4">
+        <Card className="prose" mt="5">
           <ReactMarkdown>{issue.description}</ReactMarkdown>
         </Card>
       </Box>
