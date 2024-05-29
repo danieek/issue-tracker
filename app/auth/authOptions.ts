9e3fbc49fd1cb883ada09f5ba6cb9a9ac2e60ad1
danieek <daniel.eunchong.kim@gmail.com>
2024-05-29 15:15:37 -0700

    assign issue to user

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -1,8 +1,9 @@
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/prisma/client";
+import { NextAuthOptions } from "next-auth";
 
-const authOptions = {
+const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -10,6 +11,9 @@ const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default authOptions;
